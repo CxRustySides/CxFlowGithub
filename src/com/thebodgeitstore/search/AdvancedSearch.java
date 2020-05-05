c7cd534a1adc3ff4ab95d072eb56d56d54ed35d5
Administrator <Administrator@WIN-8RC5ISV2U09>
2020-05-05 12:23:32 -0500

    Cxflow push to a protected branch 2

--- src/com/thebodgeitstore/search/AdvancedSearch.java
+++ src/com/thebodgeitstore/search/AdvancedSearch.java
@@ -2,7 +2,7 @@ package com.thebodgeitstore.search;
 
 //Author: Doug Logan
 //Website: https://www.CyberNinjas.com
-
+//CxFlowTest
 
 import com.thebodgeitstore.util.AES;
 import java.io.PrintWriter;
