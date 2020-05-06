e22ea2c61c08909f478cc15e340d30c411495581
Administrator <Administrator@WIN-8RC5ISV2U09>
2020-05-06 11:08:22 -0500

    Cxflow Strike 3

--- src/com/thebodgeitstore/search/AdvancedSearch.java
+++ src/com/thebodgeitstore/search/AdvancedSearch.java
@@ -2,7 +2,7 @@ package com.thebodgeitstore.search;
 
 //Author: Doug Logan
 //Website: https://www.CyberNinjas.com
-//CxFlowTest Strike 2
+//CxFlowTest Strike 3
 
 import com.thebodgeitstore.util.AES;
 import java.io.PrintWriter;
